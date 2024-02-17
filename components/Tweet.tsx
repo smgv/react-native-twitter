11c3a222546d2e68dd198397bb23d6312fbf60ea
smgv <gpatra1996@gmail.com>
2024-02-17 15:44:51 +0530

    tweet component added

---
+++ components/Tweet.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "@/components/Themed";
+
+const Tweet = (props: any) => {
+  const { tweet } = props;
+  console.log(tweet);
+  return (
+    <View style={styles.container}>
+      <Image src={tweet.user.image} style={styles.userImage} />
+      <View style={styles.mainContainer}>
+        <Text style={styles.userName}>{tweet.user.name}</Text>
+        <Text style={styles.userContent}>{tweet.content}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Tweet;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "lightgrey",
+    backgroundColor: "white",
+  },
+  mainContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  userName: {
+    fontWeight: "600",
+  },
+  userContent: {
+    lineHeight: 20,
+    marginTop: 5,
+  },
+});
